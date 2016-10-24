cc3e4f7c204ba6b9dba335ddf0dcc6557822a61d
Felipe Martins <thefemr@gmail.com>
2016-10-24 11:57:09 -0200

    Alguns troços de Java aí

---
+++ Java/HaruNoHana/src/banco/de/dados/DAOs.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package banco.de.dados;
+
+/**
+ * @author u16164
+ *
+ */
+import Core.*;
+
+public class DAOs {
+	
+	private static MeuPreparedStatement bd;
+	
+	public MeuPreparedStatement getBD ()
+	{
+		return DAOs.bd;
+	}
+	
+	
+
+}
