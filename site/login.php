ed89b24cb6f5c999296d1de108ded4030da36f2a
Felipe Martins <thefemr@gmail.com>
2016-11-26 13:33:37 -0200

    Revert "Revert "Add files via upload""
    This reverts commit 664968482bd41b3b62f2131fae57869e1d709eb4.

--- site/login.php
+++ site/login.php
@@ -12,6 +12,8 @@
     {
         session_destroy();
     }
+
+
 ?>
 <html>
 <head>
--- site/verificaLogin.php
+++ site/verificaLogin.php
@@ -1,5 +1,8 @@
 <?php
 	session_start();
+    if (!(isset($_SESSION['codCliente'])))
+    $_SESSION['qtdVisitas'] = 0;
+
 
 	$_SESSION['user'] = $_POST['user'];
 	$_SESSION['pword'] = $_POST['pword'];
@@ -16,7 +19,15 @@
         $sql = "Update Mesa set statusMesa = 1 where codMesa='".$_SESSION['mesas']."'";
         $stmt = sqlsrv_query($con,$sql);
         $sql = "Update Mesa set horario = GETDATE() where codMesa ='".$_SESSION['mesas']."'";
-        $stmt = sqlsrv_query($con, $sql);  
+        $stmt = sqlsrv_query($con, $sql);
+        $sql = "Update Cliente set ultimaVisita = GETDATE() where userLogin ='".$_SESSION['user']."'";
+        $stmt = sqlsrv_query($con, $sql); 
+        $sql = "SELECT qtdVisitas FROM Cliente WHERE userLogin='".$_SESSION['user']."'";
+        $stmt = sqlsrv_query($con,$sql);
+        if($linha = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_NUMERIC))
+        $_SESSION['qtdVisitas'] = $linha[0] + 1;
+        $sql = "UPDATE Cliente set qtdVisitas=".$_SESSION['qtdVisitas']." where userLogin='".$_SESSION['user']."'";
+        $stmt = sqlsrv_query($con,$sql);
 		header('Location:cardapio.php');
 		exit();
 	}
